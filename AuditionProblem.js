298749e89c2463147a089352a0e3fe2676bd0cdf
christine constantakos <christine.constantakos@libertymutualcom>
2017-07-06 16:04:15 -0400

    first attempt

--- AuditionProblem.js
+++ AuditionProblem.js
@@ -1,14 +1,43 @@
-function clearErrors() {    
-    for (var loopCounter = 0; 
-        loopCounter < document.forms["numberFun"].elements.length; 
-        loopCounter++) {
-        if (document.forms["numberFun"].elements[loopCounter]
-           .parentElement.className.indexOf("has-") != -1) {
-            
-            document.forms["numberFun"].elements[loopCounter]
-               .parentElement.className = "form-group";
-        }
-    }    
+
+function findEven(startingnumber, endingnumber, step) {
+    var findEvenNums = [];
+    for (var i = startingnumber; i< endingnumber, i += step;)
+    if (i % 2==0) {
+        return(findEvenNums);
+    }
+}
+function validateItems() {
+    clearErrors();
+    var startingnumber = document.forms["numbers"]["startingnumber"].value;
+    var endingnumber = document.forms["numbers"]["endingnumber"].value;
+    var step = document.forms["numbers"]["step"].value;
+
+    if (startingnumber == "" || isNaN(startingnumber)) {
+        alert("Starting Number must be filled in with a number.");
+        document.forms["numbers"]["startingnumber"]
+           .parentElement.className = "form-group has-error";
+        document.forms["numbers"]["startingnumber"].focus();
+        return false;
+    }
+   if (endingnumber == "" || isNaN(endingnumber)) {
+       alert("Ending Number must be filled in with a number.");
+       document.forms["numbers"]["endingnumber"]
+          .parentElement.className = "form-group has-error"
+       document.forms["numbers"]["endingnumber"].focus();
+       return false;
+   }
+     if (step == "" || step < 0 ||  isNaN(step) ) {
+       alert("Step must be filled in with a number.");
+       document.forms["numbers"]["step"]
+          .parentElement.className = "form-group has-error"
+       document.forms["numbers"]["step"].focus();
+       return false;
+     }
+
+
+   
+   return false;
+
 }
 
 
