1eb0cd5adb702b4294e3676f20613c5d9d3d36ea
christine constantakos <christine.constantakos@libertymutualcom>
2017-07-07 10:32:14 -0400

    added alerts

--- AuditionProblem.js
+++ AuditionProblem.js
@@ -1,25 +1,44 @@
 
-  function myarray() {
-    var starting = +document.getElementById("starting").value,
-        ending = +document.getElementById("ending").value,
-        step = +document.getElementById("step").value,
-        results = document.getElementById("results"),
-        i,
-        array = [];
-      
-    for (i = starting;  i <= ending; i += step) {
-        i % 2 || array.push(i);
+
+function findEvens(startingnumber, endingnumber, step) {
+    var result = 0;
+    for (var i = startingnumber; i < endingnumber; i += step) {
+        if (i % 2 == 0) {
+        }
+        retrurn(result);
     }
-    results.innerHTML = array.join(', ');
-}
-function valiateItems () {
-    var num1 = document.forms["numbers"]["num"]
 }
 
 
+    function validateItems() {
 
+        var num1 = document.forms["numbers"]["startingnumber"].value;
+        var num2 = document.forms["numbers"]["endingnumber"].value;
+        var num3 = document.forms["numbers"]["step"].value;
 
+        if (num1 == "" || isNaN(num1) || num1 <= 0) {
+            alert("Starting Number must be filled in with a positive number.");
+            document.forms["numbers"]["num1"]
+                .parentElement.className = "form-group has-error";
+            document.forms["numbers"]["num1"].focus();
+            return false;
+        }
+        if (num2 == "" || isNaN(num2) || num2 <= 0) {
+            alert("Ending Number must be filled in with a positive number.");
+            document.forms["numbers"]["num2"]
+                .parentElement.className = "form-group has-error";
+            document.forms["numbers"]["num2"].focus();
+            return false;
+        }
+        if (num3 == "" || isNaN(num3) || num3 <= 0) {
+            alert("Step Number must be filled in with a positive number.");
+            document.forms["myForm"]["num3"]
+                .parentElement.className = "form-group has-error";
+            document.forms["numbers"]["num3"].focus();
+            return false;
+        }
 
-
-
-
+    if (num2 >= num1) {
+        alert("Ending Number cannot be less than or equal to Starting Number.");
+    }
+}
