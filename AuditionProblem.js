fcfeb19ef0b2753b3d2c02f1c4d86d071ca5687b
christine constantakos <christine.constantakos@libertymutualcom>
2017-07-06 22:29:41 -0400

    added another option

--- AuditionProblem.js
+++ AuditionProblem.js
@@ -1,26 +1,18 @@
 
-function playButton(){
-run();
-return false;
+  function myarray() {
+    var starting = +document.getElementById("starting").value,
+        ending = +document.getElementById("ending").value,
+        step = +document.getElementById("step").value,
+        results = document.getElementById("results"),
+        i,
+        array = [];
+      
+    for (i = starting;  i <= ending; i += step) {
+        i % 2 || array.push(i);
+    }
+    results.innerHTML = array.join(', ');
 }
-function run(){
-var x = parseInt(document.getElementById("startingNum").value);
-var y = parseInt(document.getElementById("endingNum").value);
-var z = parseInt(document.getElementById("step").value);
-document.getElementById("startNum").innerHTML = x;
-document.getElementById("endNum").innerHTML = y;
-document.getElementById("stepNum").innerHTML = z;
-var evenNum = [];
-while (x < y){ 
-if (x%2 == 0){
-evenNum.push(x);
-} 
-x += z; 
-}
-for (var i = 0; i<evenNum.length; i++){
-document.getElementById("result").innerHTML += "<p>" + evenNum[i] + "</p>"; 
-}
-}
+
 
 
 
